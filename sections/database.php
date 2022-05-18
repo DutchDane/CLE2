c5ad21454342d619e3b6b65b55b9e5b61609dcfd
Dane Koenders <68786418+DutchDane@users.noreply.github.com>
2022-05-18 22:47:10 +0200

    Delete sections directory

--- sections/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$host       = "localhost";
-$user       = "root";
-$password   = "";
-$database   = "reservations_kaap";
-
-$db = mysqli_connect($host, $user, $password, $database)
-or die("Error: " . mysqli_connect_error());
